ba0c7c46ffa9ac925e4a8bd1be0a64944976be1d
unknown <yudhapane@yahoo.com>
2015-04-16 10:11:18 +0200

    RL2 works, but still required supervision and manual on traininig tuning

--- Robot RL2/loadParamsUR5_2.m
+++ Robot RL2/loadParamsUR5_2.m
@@ -9,7 +9,7 @@ params.theta    = zeros(params.NrbfX*params.NrbfY,1); 	% critic parameters
 params.zllim        = 0.32;                     % lower limit of the state space (position)
 params.zulim        = 0.34;                     % upper limit of the state space
 params.zdotllim 	=  -4.253e-3;             	% lower limit of the state space (velocity)
-params.zdotulim     =  6.989e-5;                % upper limit of the state space
+params.zdotulim     =  4e-3;                % upper limit of the state space
 
 % generate center of rbf (2xN matrix)
 zc          = linspace(params.zllim, params.zulim, params.NrbfX);   
@@ -18,14 +18,16 @@ zc          = reshape(zc, [1, params.NrbfX*params.NrbfY]);
 zdotc       = linspace(params.zdotllim, params.zdotulim, params.NrbfY);
 zdotc       = repmat(zdotc, [1, params.NrbfX]);
 params.c    = [zc; zdotc];                    	% center coordinate of all rbfs  
-params.B    = [7e-06 0; 0 3e-07];           	% the rbf's variance
+params.B    = [5e-07 0; 0 5e-07];           	% the rbf's variance
 
 % Other RL parameters
-params.Q        = [1e7 0; 0 1e7];          	% the error cost function penalty
+params.Q        = [1e7 0; 0 1e6];          	% the error cost function penalty
 params.R        = 1e-1;                     % the actor input cost function penalty
-params.alpha_a  = 2e-05;                     % actor learning rate
-params.alpha_c  = 0.7;                      % critic learning rate
-params.gamma    = 0.97;                     % discount term
+params.alpha_a1 = 3e-06;                    % actor learning rate
+params.alpha_a2 = 0.05;                     % actor learning rate
+params.alpha_c1 = 0.1;                      % critic learning rate
+params.alpha_c2 = 0.7;                      % critic learning rate
+params.gamma    = 0.99;                     % discount term
 params.lambda   = 0.65;                     % eligibility trace rate
 
 % Saturation parameters
@@ -37,12 +39,15 @@ params.skew     = 0.500;
 
 % Various paramaters
 params.varRand          = 2e-4; 	% exploration variance
+params.ampRand          = 4;        % exploration multiplier
+params.idxRand          = 300;       
 params.expSteps         = 3;      	% exploration steps
 params.varInitInput     = 1;        % initial input variance for each new trial
 params.expStepsRedIter  = 400;      % the exploration steps is reduced at this iteration    
 params.expVarRedIter    = 680;      % the exploration variance is reduced at this iteration
 params.expStops         = 720;    	% the exploration is stopped at this iteration
-params.plotSteps        = 1;    	% the actor, critic, td & return plot steps
+params.plotSteps        = 10;    	% the actor, critic, td & return plot steps
 params.qHome            = [-0.1921 -1.8577 2.0274 -0.1697 1.3787 3.1416]; 
 params.acc              = 10;       % default robot joint acceleration [rad/s^2]
 params.plotopt          = '2d';     % the option of the function approximators plot
+params.rlPause          = 25;
